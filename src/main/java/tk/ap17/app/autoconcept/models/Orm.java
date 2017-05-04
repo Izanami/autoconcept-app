64f233f92e95d78c20162729d65e41b859cf9004
Kélian <kelian.bousquet@laposte.net>
2017-05-04 16:32:31 +0200

    MVC & ORM

---
+++ src/main/java/tk/ap17/app/autoconcept/models/Orm.java
@@ -0,0 +1,75 @@
+package tk.ap17.app.autoconcept.models;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Orm {
+	/**
+	 * Select generique sur la bdd MySql
+	 */
+	public Map select (List<String> colums){
+
+		return Map<String,String>;
+	}
+
+	/**
+	 * Insérrer des champs
+	 */
+	public boolean insert(Map<String, String> value) {
+
+		return true;
+	}
+
+	/**
+	 * Modifier des champs
+	 */
+	public boolean update(Map<String, String> value) {
+
+		return true;
+	}
+
+	/**
+	 * Supprimer des champs
+	 */
+	public boolean delete(Map<String, String> value) {
+
+		return true;
+	}
+
+	/**
+	 * Récupérer une table
+	 */
+	public String getTable() {
+
+		return "";
+	}
+
+	/**
+	 * Récupérer une liste des tables
+	 * 
+	 * @param name
+	 */
+	public List setTable(String name) {
+
+		return 
+	}
+
+	/**
+	 * 
+	 */
+	public List getColums() {
+
+		return
+	}
+
+	/**
+	 * 
+	 */
+	public List setColums() {
+
+		return
+	}
+}

c5d6994e77c4ff22c65150e6adc13b4e8575f561
MYKIE40PC\mykie40 <mykie40@gmail.com>
2017-05-05 12:07:20 +0200

    methode main d'affichage des 3 variables uri, user, password

--- src/main/java/tk/ap17/app/autoconcept/views/Debug.java
+++ src/main/java/tk/ap17/app/autoconcept/views/Debug.java
@@ -1,5 +1,35 @@
 package tk.ap17.app.autoconcept.views;
 
 public class Debug {
+	
+	String uri;
+	String user;
+	String password;
+	
+	public static void main(String[] args) {
+		
+		Debug debug = new Debug();
+		
+		debug.uri = "BEAUREGARD";
+		
+		debug.user = "jack";
+		debug.password = "classe";
+		
+		display(debug.uri);
+		display(debug.user);
+		display(debug.password);
+		
+		
+		
+		
+		
+	}
 
+	
+	
+	public static void display(String message){
+		
+		System.out.println(message);
+		
+	}
 }

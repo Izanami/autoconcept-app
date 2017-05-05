fcb3c3d55214e48b8a5a046122d4f07f771ccd17
Kélian <kelian.bousquet@laposte.net>
2017-05-05 12:34:21 +0200

    ajout des constructeur

---
+++ src/main/java/tk/ap17/app/autoconcept/models/Connector.java
@@ -0,0 +1,47 @@
+package tk.ap17.app.autoconcept.models;
+
+import java.nio.file.Path;
+import java.sql.*;
+
+public class Connector {
+
+	/**
+	 * Constructeurs
+	 */
+	public Connector() {
+
+	}
+
+	public Connector(String uri) {
+
+	}
+
+	public Connector(String uri, String user, String password) {
+
+	}
+
+	public Connector(Path uri) {
+
+	}
+
+	public Connector(Path uri, String user, String password) {
+
+	}
+
+	/**
+	 * Méthodes : connexion, execution, deconnexion
+	 */
+
+	public void connect() {
+
+	}
+
+	public void execute(String query) {
+
+	}
+
+	public void close() {
+
+	}
+
+}

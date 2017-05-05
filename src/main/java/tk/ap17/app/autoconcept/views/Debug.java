d61b957b8042d3ab5916b64dca5db86a12d230af
MYKIE40PC\mykie40 <mykie40@gmail.com>
2017-05-05 12:29:49 +0200

    Change

--- src/main/java/tk/ap17/app/autoconcept/views/Debug.java
+++ src/main/java/tk/ap17/app/autoconcept/views/Debug.java
@@ -1,26 +1,28 @@
 package tk.ap17.app.autoconcept.views;
 
+import tk.ap17.app.autoconcept.models.Connecteur;
+
 public class Debug {
 	
+	Connecteur connecteur;
+	
+	/*
 	String uri;
 	String user;
 	String password;
-	
+	*/
 	public static void main(String[] args) {
 		
-		Debug debug = new Debug();
+		/*Debug debug = new Debug();
 		
-		debug.uri = "BEAUREGARD";
-		
-		debug.user = "jack";
-		debug.password = "classe";
+		debug.uri = "URI: 	  BEAUREGARD";
+		debug.user = "USER: 	  jack";
+		debug.password = "PASSWORD: classe";
 		
 		display(debug.uri);
 		display(debug.user);
 		display(debug.password);
-		
-		
-		
+		*/
 		
 		
 	}

6d91f1e85863bc1bc7a14e1711d7c4bf70653cd9
Adrien Jeser <adrien@jeser.me>
2017-05-05 13:46:03 +0200

    Merge Connecteur and Connector

--- src/main/java/tk/ap17/app/autoconcept/models/Connector.java
+++ src/main/java/tk/ap17/app/autoconcept/models/Connector.java
@@ -4,44 +4,84 @@ import java.nio.file.Path;
 import java.sql.*;
 
 public class Connector {
+    private String uri; // Uniform Resource Identifier
+    private String user; // Identifiant utilisateur
+    private String password; // Mot de passe
+    private String port; // Port de connection
 
-	/**
-	 * Constructeurs
-	 */
-	public Connector() {
+    /**
+     * Constructeurs
+     */
+    public Connector() {
 
-	}
+    }
 
-	public Connector(String uri) {
+    public Connector(String uri) {
 
-	}
+    }
 
-	public Connector(String uri, String user, String password) {
+    public Connector(String uri, String user, String password) {
 
-	}
+    }
 
-	public Connector(Path uri) {
+    public Connector(Path uri) {
 
-	}
+    }
 
-	public Connector(Path uri, String user, String password) {
+    public Connector(Path uri, String user, String password) {
 
-	}
+    }
 
-	/**
-	 * Méthodes : connexion, execution, deconnexion
-	 */
+    /**
+     * Méthodes : connexion, execution, deconnexion
+     */
 
-	public void connect() {
+    public void connect() {
 
-	}
+    }
 
-	public void execute(String query) {
+    public void execute(String query) {
 
-	}
+    }
 
-	public void close() {
+    public void close() {
 
-	}
+    }
+
+    /* Accesseurs à l'attribut uri ******************************/
+    public String getUri() {
+        return uri;
+    }
+
+    void setUri(String newUri) {
+        uri = newUri;
+    }
+
+    /* Accesseurs à l'attribut user *****************************/
+    public String getUser() {
+        return user;
+    }
+
+    void setUser(String newUser) {
+        user = newUser;
+    }
+
+    /* Accesseurs à l'attribut password *************************/
+    public String getPassword() {
+        return password;
+    }
+
+    void setPassword(String newPassword) {
+        password = newPassword;
+    }
+
+    /* Accesseurs à l'attribut port *****************************/
+    public String getPort() {
+        return port;
+    }
+
+    void setPort(String newPort) {
+        port = newPort;
+    }
 
 }

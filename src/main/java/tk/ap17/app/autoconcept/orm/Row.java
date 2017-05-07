161825ab4ff76de4c1a925e8e8b7c82a4430f779
Adrien Jeser <adrien@jeser.me>
2017-05-07 18:17:46 +0200

    Row

---
+++ src/main/java/tk/ap17/app/autoconcept/orm/Row.java
@@ -0,0 +1,19 @@
+package tk.ap17.app.autoconcept.orm;
+
+public class Row {
+    private String primaryKey;
+
+    /**
+     * @return the primaryKey
+     */
+    public String getPrimaryKey() {
+        return primaryKey;
+    }
+
+    /**
+     * @param primaryKey the primaryKey to set
+     */
+    public void setPrimaryKey(String primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+}

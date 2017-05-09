a917fb86dec23a1f7f2b8dbf3709458d5fc9ad11
Kélian <kelian.bousquet@laposte.net>
2017-05-09 13:42:51 +0200

    mise en place des boutons pour le controller contact

--- src/main/java/tk/ap17/app/autoconcept/controllers/ContactController.java
+++ src/main/java/tk/ap17/app/autoconcept/controllers/ContactController.java
@@ -2,6 +2,10 @@ package tk.ap17.app.autoconcept.controllers;
 
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import java.io.IOException;
@@ -19,7 +23,61 @@ public class ContactController extends Controller {
 
 	@FXML
 	private ImageView retour;
-
+	
+	@FXML
+	private ChoiceBox<?> choixCategorie;
+	
+	@FXML
+	private Label entreprise;
+	
+	@FXML
+	private ChoiceBox<?> choixEntreprise;
+	
+	@FXML
+	private TextField whereNom;
+	
+	@FXML
+	private TextField wherePrenom;
+	
+	@FXML
+	private TextField whereDdn;
+	
+	@FXML
+	private Label fsCat;
+	
+	@FXML
+	private Label fsAdresse;
+	
+	@FXML
+	private Label fsNom;
+	
+	@FXML
+	private Label fsPrenom;
+	
+	@FXML
+	private Label fsDdn;
+	
+	@FXML
+	private Label fsSexe;
+	
+	@FXML
+	private Label fsCourriel;
+	
+	@FXML
+	private Label fsTelephone;
+	
+	@FXML
+	private Label fsInscription;
+	
+	@FXML
+	private Button nouveau;
+	
+	@FXML
+	private Button editer;
+	
+	@FXML
+	private Button supprimer;
+	
 
 	public void retourAccueil(MouseEvent event) {
 		try {

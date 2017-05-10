9baea8ff1b70d577b2467362140f65886879c4d9
Kélian <kelian.bousquet@laposte.net>
2017-05-10 12:06:16 +0200

    boite de dialogue éditer

--- src/main/java/tk/ap17/app/autoconcept/App.java
+++ src/main/java/tk/ap17/app/autoconcept/App.java
@@ -8,10 +8,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -139,6 +141,44 @@ public class App extends Application {
 			exceptionDialog(e, "An exception was throw.");
 		}
 	}
+	
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     *
+     * @param person the contact object to be edited
+     * @return true if the user clicked OK, false otherwise.
+     */
+//    public boolean showPersonEditDialog(Contacts contact) {
+//        try {
+//            // Load the fxml file and create a new stage for the popup dialog.
+//            FXMLLoader loader = new FXMLLoader();
+//            loader.setLocation(App.class.getResource("PersonEditDialog.fxml"));
+//            AnchorPane page = (AnchorPane) loader.load();
+//
+//            // Create the dialog Stage.
+//            Stage dialogStage = new Stage();
+//            dialogStage.setTitle("Edit Person");
+//            dialogStage.initModality(Modality.WINDOW_MODAL);
+//            dialogStage.initOwner(primaryStage);
+//            Scene scene = new Scene(page);
+//            dialogStage.setScene(scene);
+//
+//            // Set the person into the controller.
+//            PersonEditDialogController controller = loader.getController();
+//            controller.setDialogStage(dialogStage);
+//            controller.setPerson(contact);
+//
+//            // Show the dialog and wait until the user closes it
+//            dialogStage.showAndWait();
+//
+//            return controller.isOkClicked();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
 
 	
 	/**
